0628060d8c3cd0cc0257d3835044b6d4c3d0c424
Michael Loughman <mcloughman@gmail.com>
2023-10-26 09:53:00 -0700

    Add static login method to userModel. Implement the method in userController and test in Postman.

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,17 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({ msg: "Login user" })
+  const { email, password } = req.body
+  try {
+    // signup is the static method we created in our userModel
+    const user = await User.login(email, password)
+
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
 // signup user
--- models/userModel.js
+++ models/userModel.js
@@ -38,5 +38,21 @@ userSchema.statics.signup = async function (email, password) {
   const user = await this.create({ email, password: hash })
   return user
 }
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled")
+  }
+  const user = await this.findOne({ email })
+  if (!user) {
+    throw Error("Incorrect email")
+  }
+  const match = await bcrypt.compare(password, user.password)
+  if (!match) {
+    throw Error("Incorrect password")
+  }
+  return user
+}
 const User = mongoose.model("Individual", userSchema)
 export default User
